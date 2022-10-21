d8b3a3ff4de84dd63635094f64d7ddb7440a35da
Ying Ying <yingying@KuangYesMacBook.attlocal.net>
2022-10-20 23:09:24 -0700

    test  node index js in terminal

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // TODO: Create an array of questions for user input
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 const questions = [
 inquirer
